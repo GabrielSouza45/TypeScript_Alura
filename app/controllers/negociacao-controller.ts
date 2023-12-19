621b012ac69dd9fe4b244a9da7ba59a4fd03b2e7
Gabriel Freitas Souza <gabrielsouza45@live.com>
2023-12-19 14:59:40 -0300

    Finalização_2.4

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -10,7 +10,7 @@ export class NegociacaoController {
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
-    private negociacoesView = new NegociacoesView('#negociacoesView');
+    private negociacoesView = new NegociacoesView('#negociacoesView', true);
     private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
@@ -21,21 +21,17 @@ export class NegociacaoController {
     }
 
     adiciona(): void {
-        const negociacao = this.criaNegociacao();
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputData.value,
+            this.inputData.value
+        );
+
         if (this.validaData(negociacao)){
             this.negociacoes.adiciona(negociacao);
             this.atualizaView();
             this.limparFormulario();
         }
-    }
-
-    private criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const data = new Date(this.inputData.value.replace(exp, ","));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-
-        return new Negociacao( data, quantidade, valor );
     }
 
     private limparFormulario(): void {
